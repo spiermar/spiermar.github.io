9c70a31f6ec0fdb789adb30e4c25c092f638429b
Martin Spier <spiermar@gmail.com>
2018-10-13 15:33:59 -0700

    feat: contact form

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,6 @@
-jQuery(document).ready(function( $ ) {
-
-	// Menu settings
-  var initgnMenu = new gnMenu(document.getElementById('gn-menu'));
-
-  // Carousel
-  $('.carousel').carousel({
-    interval: 5500
-  })
-
-	// Smooth scroll for the menu and links with .scrollto classes
-  $('.smoothscroll').on('click', function(e) {
+function initScroll() {
+  // smooth scroll for the menu and links with .scrollto classes
+  $('.smoothscroll').on('click', function (e) {
     e.preventDefault();
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -22,46 +13,16 @@ jQuery(document).ready(function( $ ) {
       }
     }
   });
+}
 
-  // Charts
-  if($('#canvas').length) {
+function initMenu() {
+  // menu
+  var initgnMenu = new gnMenu(document.getElementById('gn-menu'));
+}
 
-		var doughnutData = [{
-        value: 70,
-        color: "#3bc492"
-      },
-      {
-        value: 30,
-        color: "#ecf0f1"
-      }
-    ];
-    var myDoughnut = new Chart(document.getElementById("canvas").getContext("2d")).Doughnut(doughnutData);
-	};
-
-	if($('#canvas1').length) {
-		var doughnutData = [{
-				value: 90,
-				color: "#3bc492"
-			},
-			{
-				value: 10,
-				color: "#ecf0f1"
-			}
-		];
-		var myDoughnut = new Chart(document.getElementById("canvas1").getContext("2d")).Doughnut(doughnutData);
-	}
-
-	if($('#canvas2').length) {
-		var doughnutData = [{
-				value: 55,
-				color: "#3bc492"
-			},
-			{
-				value: 45,
-				color: "#ecf0f1"
-			}
-		];
-		var myDoughnut = new Chart(document.getElementById("canvas2").getContext("2d")).Doughnut(doughnutData);
-	}
-
-});
+function initTestimonialsCarousel() {
+  // testimonials
+  $('.carousel').carousel({
+    interval: 5500
+  })
+}
---
+++ public/js/message.js
@@ -0,0 +1,61 @@
+function initMessageForm() {
+  const FORMSPREE_FORM = 'mqpbgpag';
+  const FORMSPREE_POST_URL = `https://formspree-martinspierio.herokuapp.com/${FORMSPREE_FORM}`;
+
+  const FORM_ELEMENT = document.getElementById('contact-form');
+  const SUCCESS_ELEMENT = document.getElementById('sent-message');
+  const ERROR_ELEMENT = document.getElementById('error-message');
+
+  const formDataToJson = (formData) => {
+    const entries = formData.entries();
+    const dataObj = Array.from(entries).reduce((data, [key, value]) => {
+      data[key] = value;
+      if (key === 'email') {
+        data._replyTo = value;
+      }
+      if (key === 'subject') {
+        data._subject = value;
+      }
+      return data;
+    }, {});
+    return JSON.stringify(dataObj);
+  };
+
+  const toggleSuccess = (bool) => {
+    if (bool) {
+      SUCCESS_ELEMENT.style.display = 'block';
+    } else {
+      SUCCESS_ELEMENT.style.display = 'none';
+    }
+  }
+
+  FORM_ELEMENT.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(this);
+
+    fetch(FORMSPREE_POST_URL, {
+        method: 'POST',
+        body: formDataToJson(formData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .catch((err) => {
+        ERROR_ELEMENT.innerHTML = 'Something went wrong. Please try again later.'
+        ERROR_ELEMENT.style.display = 'block';
+        console.log(err);
+      })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.success === 'email sent') {
+          toggleSuccess(true);
+          FORM_ELEMENT.reset();
+        } else {
+          ERROR_ELEMENT.innerHTML = 'Something went wrong. Please try again later.'
+          ERROR_ELEMENT.style.display = 'block';
+          console.log('Oops. Something went wrong.')
+        }
+      });
+  });
+}
